4559c93c9554dbbd0172603038a9ddf3e470fbb0
Guilherme Souza <159392971+gui6y@users.noreply.github.com>
2024-02-08 18:10:31 -0300

    Opção de sair do programa.
    Adicionamos uma opção para sair do programa no menu.

--- Cartorio.c
+++ Cartorio.c
@@ -19,7 +19,8 @@ int main()
 		printf("Escolha a opção desejada no menu:\n\n");
 		printf("\t1 - Registrar nomes\n"); // o comando \t = espaçamento
 		printf("\t2 - Consultar nomes\n");
-		printf("\t3 - Deletar nomes\n\n"); 
+		printf("\t3 - Deletar nomes\n"); 
+		printf("\t4 - Sair do programa\n\n");
 		printf("Opção: "); //fim do menu
 		
 		scanf("%d" , &opcao); //armazenando a escolha do usuario
@@ -36,6 +37,10 @@ int main()
 				break;
 			case 3:
 				deletar();
+				break;
+			case 4:
+				printf("Obrigado por utilizar o sistema!");
+				return 0;
 				break;
 			default:
 				printf("Essa opção não existe.\n");
